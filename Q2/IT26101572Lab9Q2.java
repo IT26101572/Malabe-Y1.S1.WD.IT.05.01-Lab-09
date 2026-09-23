59abf9eda3f1aec33e273c8ad5f9eb733c47ff54
IT26101572 <it26101572@my.sliit.lk>
2026-09-23 10:29:22 +0530

    Add files via upload

---
+++ Q2/IT26101572Lab9Q2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class IT26101572Lab9Q2 {
+
+    public static double circleArea(double radius) {
+        double area = Math.PI * Math.pow(radius, 2);
+        return area;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the radius of the circle: ");
+        double radius = input.nextDouble();
+
+        double area = circleArea(radius);
+
+        System.out.println("The area of the circle with radius "
+                + radius + " is: " + area);
+
+        input.close();
+    }
+}
